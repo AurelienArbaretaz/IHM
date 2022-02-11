21ff9430242c88273d74998a5afdee80fc00e34b
max <max.shirugan@gmail.com>
2022-02-11 17:13:56 +0100

    Ajout du form + CSS

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -11,5 +11,28 @@ export class ContactFormComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  IsSend: boolean = false;
+  mail: string = "";
+  mailSend: string = "";
+  content: string = "";
+  contentSend: string = "";
+  error: string = "";
+  isError: boolean = false;
+  send(): void {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.mail) == false || this.mail =="") {
+      this.error = "Veuillez entrer une adresse mail valide";
+      this.isError = true;
+    }
+    else if (this.content == "") {
+      this.error = "Veuillez entrer un message";
+      this.isError = true;
+    }
+    else {
+      this.isError = false;
+        this.IsSend = true;
+        this.mailSend = this.mail;
+        this.contentSend = this.content;
+    }
 
+  }
 }
